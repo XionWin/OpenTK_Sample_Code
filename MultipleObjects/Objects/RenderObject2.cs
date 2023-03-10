60c1aa3bb8d05b598e6a58722d7840bd238a5fa3
Xion <xionwin@hotmail.com>
2023-03-11 00:12:16 +0800

    Update

--- MultipleObjects/Objects/RenderObject2.cs
+++ MultipleObjects/Objects/RenderObject2.cs
@@ -1,11 +1,6 @@
 ﻿using Common;
 using OpenTK.Graphics.OpenGL4;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultipleObjects.Objects
 {
